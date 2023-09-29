aaa22a796e35aa1d64067b7ab38f1f60177758c3
miguelzynich@gmail.com <miguelzynich@gmail.com>
2023-09-29 11:02:22 -0300

    tarefa_ok

---
+++ src/ArrayCarros.js
@@ -0,0 +1,14 @@
+import carros from './carros.js';
+
+const precos = carros.map(carro => carro.preco);
+console.log(precos);
+
+const precoTotal = carros.reduce((total, carro) => total + carro.preco, 0);
+console.log(precoTotal);
+
+const carrosCaros = carros.filter(carro => carro.preco > 22000);
+console.log(carrosCaros);
+
+carros.forEach(carro => {
+    console.log(`Marca: ${carro.marca}, Modelo: ${carro.modelo}`);
+});
---
+++ src/carros.js
@@ -0,0 +1,26 @@
+const carros = [
+    {
+        marca: 'Toyota',
+        modelo: 'Corolla',
+        ano: 2020,
+        preco: 25000
+    },
+    {
+        marca: 'Honda',
+        modelo: 'Civic',
+        ano: 2019,
+        preco: 22000
+    },
+    {
+        marca: 'Ford',
+        modelo: 'Focus',
+        ano: 2021,
+        preco: 27000
+    },
+    {
+        marca: 'Chevrolet',
+        modelo: 'Cruze',
+        ano: 2018,
+        preco: 20000
+    }
+];
